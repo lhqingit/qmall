b4f3d1ffb70f0aeb037c0da921027831701c3737
lhqingit <lhq_ing@qq.com>
2018-03-18 09:26:24 +0800

    FreeMarker练习

---
+++ qmall-item-web/src/test/java/net/imwork/lhqing/qmall/freemarker/Student.java
@@ -0,0 +1,51 @@
+package net.imwork.lhqing.qmall.freemarker;
+/**
+ * 
+ * @author lhq_i
+ * Q:测试使用FreeMarker模板取POJO的实例Bean
+ */
+public class Student {
+
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	//Q:构造方法
+	public Student(int id, String name, int age, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	public Student() {}
+	
+	
+	
+}
